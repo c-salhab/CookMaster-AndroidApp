50d48ed18aba2433e2c6d27e5d888c0a6defc479
csalhabb <charbel.salhab.2003@gmail.com>
2023-07-03 08:24:03 +0200

    fix files of db connection and login

--- DataBase.php
+++ DataBase.php
@@ -52,20 +52,5 @@ class DataBase
         return $login;
     }
 
-    function signUp($table, $fullname, $email, $username, $password)
-    {
-        $fullname = $this->prepareData($fullname);
-        $username = $this->prepareData($username);
-        $password = $this->prepareData($password);
-        $email = $this->prepareData($email);
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $this->sql =
-            "INSERT INTO " . $table . " (fullname, username, password, email) VALUES ('" . $fullname . "','" . $username . "','" . $password . "','" . $email . "')";
-        if (mysqli_query($this->connect, $this->sql)) {
-            return true;
-        } else return false;
-    }
-
 }
 
-?>
--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 require "DataBase.php";
+
 $db = new DataBase();
+
 if (isset($_POST['username']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
         if ($db->logIn("users", $_POST['username'], $_POST['password'])) {
@@ -8,4 +10,4 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         } else echo "Username or Password wrong";
     } else echo "Error: Database connection";
 } else echo "All fields are required";
-?>
+
